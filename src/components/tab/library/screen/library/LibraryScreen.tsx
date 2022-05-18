a96108756fc85edc283684165f851aff0660221e
HIRO15254 <hi089697@gmail.com>
2022-05-18 16:08:19 +0900

    所持問題集の一覧を取得・表示

--- src/components/tab/library/screen/library/LibraryScreen.tsx
+++ src/components/tab/library/screen/library/LibraryScreen.tsx
@@ -1,26 +1,41 @@
 import React, { useState } from 'react';
-import { ScrollView, View, } from 'native-base';
+import { ScrollView, View, Heading, FlatList, Box, Text } from 'native-base';
 import AddBookButton from './parts/AddBookButton';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { LibraryStackParamList } from '../../LibraryTab';
-import { collection } from 'firebase/firestore';
-import { store, auth } from '../../../../../config/firebase';
+import readBook from '../../../../../script/readBook';
+import Book from '../../../../../script/class/Book';
 
 type Props = NativeStackScreenProps<LibraryStackParamList, 'Library'>
 
 const HomeScreen = ({ navigation } :Props) => {
-  const [books, setBooks] = useState({});
+  const [books, setBooks] = useState<Book[]>([]);
+
+  React.useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      getBookData();
+    });
+    // Return the function to unsubscribe from the event so it gets removed on unmount
+    return unsubscribe;
+  }, [navigation]);
+
 
   const pressAddBookButton = () => {
     navigation.navigate("CreateBook");
   }
 
   const getBookData = async () => {
-    const ref = collection(store, "users", auth.currentUser?.uid!, "books")
+    setBooks(await readBook());
   }
 
   return (
     <View flex={1}>
+      <FlatList data={books} renderItem={({item}) => 
+        <Box>
+          <Text>{item.title}</Text>
+        </Box>
+      }>
+      </FlatList>
       <AddBookButton onPress={pressAddBookButton} />
     </View>
   );
--- src/script/createBook.tsx
+++ src/script/createBook.tsx
@@ -2,19 +2,15 @@ import { auth, store } from '../config/firebase';
 import { addDoc, collection } from 'firebase/firestore';
 
 const createBook = async (title: string, author: string, description: string) => {
-  try {
-    if(typeof(auth.currentUser?.uid) == "string") {
-      const uid = auth.currentUser?.uid;
-      addDoc(collection(store, "users", uid, "books"), {
-        title: title,
-        author: author,
-        description: description,
-      });
-    } else {
-      throw Error("ログインしていません。アプリケーションを再起動してログインしてください。");
-    }
-  } catch(e) {
-    throw e;
+  if(typeof(auth.currentUser?.uid) == "string") {
+    const uid = auth.currentUser?.uid;
+    addDoc(collection(store, "users", uid, "books"), {
+      title: title,
+      author: author,
+      description: description,
+    });
+  } else {
+    throw Error("ログインしていません。アプリケーションを再起動してログインしてください。");
   }
 }
 
