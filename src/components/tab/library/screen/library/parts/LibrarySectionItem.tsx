944e0f7633abd4cacb112ff1f7015328402936a6
HIRO15254 <hi089697@gmail.com>
2022-05-30 20:44:29 +0900

    セクションに対しての操作を行うアクションシートの設置

--- src/components/tab/library/screen/library/parts/LibrarySectionItem.tsx
+++ src/components/tab/library/screen/library/parts/LibrarySectionItem.tsx
@@ -1,27 +1,40 @@
 import React from 'react';
 import {Pressable, HStack, Center, Text, VStack, Spacer} from 'native-base';
 import Section from '../../../../../../script/class/Section';
+import SectionActionButton from './SectionActionButton';
 
 type LibrarySectionItemProps = {
   bookId: string
   id: string
   data: Section
+  reload: () => Promise<void>
 }
 
-const LibrarySectionItem: React.VFC<LibrarySectionItemProps> = ({id, data}) => {
+const LibrarySectionItem: React.VFC<LibrarySectionItemProps> = (
+    {bookId, id, data, reload},
+) => {
   return (
     <Pressable>
       <HStack
         backgroundColor='blue.100'
         borderBottomWidth={1}
         borderBottomColor="gray.400"
-        p={1}
+        p={0.5}
       >
         <Center>
           <Text fontSize="lg" bold m={1}>{data.title}</Text>
         </Center>
         <Spacer />
-        <VStack />
+        <VStack>
+          <SectionActionButton
+            item={{
+              BookId: bookId,
+              id: id,
+              value: data,
+            }}
+            reload={reload}
+          />
+        </VStack>
       </HStack>
     </Pressable>
   );
--- src/components/tab/library/screen/library/parts/SectionActionButton.tsx
+++ src/components/tab/library/screen/library/parts/SectionActionButton.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import {View, Button, Icon, Actionsheet, useDisclose} from 'native-base';
 import {Ionicons} from '@expo/vector-icons';
-import Book from '../../../../../../script/class/Book';
-import DeleteBookModal from './DeleteBookModal';
 import {useNavigation} from '@react-navigation/native';
 import {LibraryStackParamList} from '../../../LibraryTab';
+import Section from '../../../../../../script/class/Section';
 import {
   NativeStackNavigationProp,
 } from '@react-navigation/native-stack/lib/typescript/src/types';
 
-type BookActionButtonProps = {
+type SectionActionButtonProps = {
   item: {
-    key: string,
-    value: Book,
+    BookId: string,
+    id: string,
+    value: Section,
   }
   reload: () => Promise<void>
 }
@@ -20,46 +20,33 @@ type BookActionButtonProps = {
 type NavigationProps = NativeStackNavigationProp<LibraryStackParamList>;
 
 /**
- * 問題集一覧における問題集に対する操作を行うボタンとアクションシート
+ * 問題集一覧におけるセクションに対する操作を行うボタンとアクションシート
  */
-const BookActionButton: React.VFC<BookActionButtonProps> = ({item, reload}) => {
+const SectionActionButton: React.VFC<SectionActionButtonProps> = (
+    {item, reload},
+) => {
   const {isOpen, onOpen, onClose} = useDisclose();
   const navigation = useNavigation<NavigationProps>();
 
   const onPressUpdateBookButton = React.useCallback(() => {
-    navigation.navigate(
-        'UpdateBook',
-        {id: item.key, book: item.value},
-    );
-  }, [item]);
-
-  const onPressCreateSectionButton = React.useCallback(() => {
-    navigation.navigate('CreateSection', {id: item.key});
-    onClose();
   }, [item]);
 
   return (
     <View>
       <Button variant="ghost" colorScheme="gray" onPress={onOpen}>
-        <Icon as={Ionicons} name="ellipsis-vertical" size={5} color="black" />
+        <Icon as={Ionicons} name="ellipsis-vertical" size={4} color="black" />
       </Button>
       <Actionsheet isOpen={isOpen} onClose={onClose}>
         <Actionsheet.Content>
-          <Actionsheet.Item
-            onPress={onPressCreateSectionButton}
-          >
-            セクションの追加
-          </Actionsheet.Item>
           <Actionsheet.Item
             onPress={onPressUpdateBookButton}
           >
             情報の編集
           </Actionsheet.Item>
-          <DeleteBookModal item={item} reload={reload}/>
         </Actionsheet.Content>
       </Actionsheet>
     </View>
   );
 };
 
-export default BookActionButton;
+export default SectionActionButton;
